b44657932d3a1ba095601b2fae2f73c83c4f4707
Connor Hasting <guardianx515@gmail.com>
2024-01-29 12:52:18 -0600

    Test commit

--- src/com/sparklefish/TwoSum.java
+++ src/com/sparklefish/TwoSum.java
@@ -3,7 +3,6 @@ package com.sparklefish;
 public class TwoSum {
 
   public int[] twoSum(int[] nums, int target) {
-
     int[] solution = new int[0];
 
     for (int i = 0; i < nums.length - 1; i++) {
